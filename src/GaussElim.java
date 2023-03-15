f5519bf7ca21c78453ae151360b80b4f38a3b0a7
Denis Soldatenko <soldatenko911@gmail.com>
2023-03-15 12:03:37 +0200

    Initial commit

---
+++ src/GaussElim.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+
+public class GaussElim {
+    private final JTextField[][] fields;
+
+    public GaussElim(JTextField[][] textFields) {
+        this.fields = textFields;
+    }
+
+    public boolean exceptionMessage(double[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                try {
+                    m[i][j] = Double.parseDouble(fields[i][j].getText());
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Invalid value: "
+                            + fields[i][j].getText(), "Error", JOptionPane.ERROR_MESSAGE);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public double[] GaussMethod(double[][] m, double[] sol) {
+        // Apply Gauss elimination
+        for (int i = 0; i < m.length - 1; i++) {
+            for (int j = i + 1; j < m.length; j++) {
+                double factor = m[j][i] / m[i][i];
+                for (int k = i; k < m[i].length; k++) {
+                    m[j][k] -= factor * m[i][k];
+                }
+            }
+        }
+
+        // Back-substitution
+        for (int i = m.length - 1; i >= 0; i--) {
+            double sum = 0;
+            for (int j = i + 1; j < m.length; j++) {
+                sum += m[i][j] * sol[j];
+            }
+            sol[i] = (m[i][m.length] - sum) / m[i][i];
+        }
+        return round(sol);
+    }
+
+    public void showResult(JTextArea textArea, double det, double[] solution) {
+        textArea.setText(String.format("Determinant = %.2f%n", det)
+                + String.format("x = %.2f%n", solution[0])
+                + String.format("y = %.2f%n", solution[1])
+                + String.format("z = %.2f", solution[2]));
+    }
+
+    public double[] round(double[] arr) {
+        double[] result = new double[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = Math.round(arr[i] * 1000.0) / 1000.0;
+        }
+        return result;
+    }
+
+    public double determinant(double[][] m) {
+        return m[0][0] * m[1][1] * m[2][2] + m[0][1] * m[1][2] * m[2][0] + m[0][2] * m[1][0] * m[2][1]
+                - m[0][2] * m[1][1] * m[2][0] - m[0][0] * m[1][2] * m[2][1] - m[0][1] * m[1][0] * m[2][2];
+    }
+}
---
+++ src/GaussGUI.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GaussGUI implements ActionListener {
+    private final JTextField[][] fields = new JTextField[3][4];
+    private final JTextArea resultArea;
+
+    public GaussGUI() {
+        JFrame frame = new JFrame("Gaussian elimination calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        JPanel inputPanel = new JPanel(new GridLayout(3, 4, 5, 5));
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+        JPanel resultPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                JTextField field = new JTextField();
+                field.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+                fields[i][j] = field;
+
+                if (j < 3) {
+                    inputPanel.add(field);
+                    inputPanel.add(new JLabel(j == 0 ? "x" : j == 1 ? "y" : "z"));
+                    if (j < 2) {
+                        inputPanel.add(new JLabel("+"));
+                    }
+                } else {
+                    inputPanel.add(new JLabel("="));
+                    inputPanel.add(field);
+                }
+            }
+        }
+
+        JButton solveButton = new JButton("Solve");
+        solveButton.setForeground(Color.decode("#000000"));
+        solveButton.setFont(new Font("Comic Sans MS", Font.PLAIN, 25));
+        solveButton.addActionListener(this);
+        solveButton.setFocusable(false);
+        buttonPanel.add(solveButton);
+
+        resultArea = new JTextArea(4, 28);
+        resultArea.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
+        resultArea.setEditable(false);
+        resultPanel.add(resultArea);
+
+        frame.add(inputPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.add(resultPanel, BorderLayout.NORTH);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        GaussElim gauss = new GaussElim(fields);
+        double[][] matrix = new double[3][4];
+        double[] solution = new double[3];
+
+        if (gauss.exceptionMessage(matrix)) return;
+
+        if (gauss.determinant(matrix) != 0.0) {
+            solution = gauss.GaussMethod(matrix, solution);
+            gauss.showResult(resultArea, gauss.determinant(matrix), solution);
+        } else {
+            JOptionPane.showMessageDialog(null, "Determinant should not be equal to zero",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
